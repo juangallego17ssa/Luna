3cc2fbee12c0771c5b13f0eee5de80ef679bc185
Laysa Uchoa <laysa.uchoa@gmail.com>
2023-04-03 11:36:20 +0200

    Add banner

---
+++ frontend/src/Componets/userProfile/Banner/banner.styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import img1 from "../../../Assets/images/b1.png";
+
+export const BannerElements = styled.div`
+  position: relative;
+  display: flex;
+  width: 100vw;
+  height: 20vh;
+  flex-direction: row;
+  
+`;
+
+export const Img = styled.img.attrs({
+  src: `${img1}`,
+})`
+  display: flex;
+
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  object-fit: cover; /* or object-fit: contain; */
+  max-width: 100%;
+  z-index: 0;
+`;
--- frontend/src/Pages/userPages/userProfile/userProfile.js
+++ frontend/src/Pages/userPages/userProfile/userProfile.js
@@ -1,19 +1,17 @@
 import { React } from "react";
-import {
-  UserProfileDiv,
-  UserProfileBody,
-} from "./userProfile.styled"
+import { UserProfileDiv, UserProfileBody } from "./userProfile.styled";
 
-import Navbar from "../../../Componets/Layout/Header/navbar"
+import Navbar from "../../../Componets/Layout/Header/navbar";
 import Footer from "../../../Componets/Layout/Footer/footer";
-
+import Banner from "../../../Componets/userProfile/Banner/banner";
 
 const UserProfile = () => {
   return (
     <UserProfileDiv>
       <Navbar />
+      <Banner></Banner>
       <UserProfileBody>
-        <h1>userProfile</h1>
+        <h1>userProfile 2</h1>
       </UserProfileBody>
       <Footer />
     </UserProfileDiv>
