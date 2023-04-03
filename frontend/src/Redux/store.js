7ab82d89abeed38d5567089c6405a6a4a582ad7f
Laysa Uchoa <laysa.uchoa@gmail.com>
2023-04-03 11:47:12 +0200

    Remove double import

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import currentUser from './Slices/currentUser'
 import signUpEmailAddress from "./Slices/signUpEmailAdress";
-import currentUser from "./Slices/currentUser";
 import userProfile from "./Slices/userProfile";
 
 export default configureStore({
