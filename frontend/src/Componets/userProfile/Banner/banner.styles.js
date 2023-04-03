3407a45c087dccbd20254395824ed234c73f900b
Laysa Uchoa <laysa.uchoa@gmail.com>
2023-04-03 15:00:33 +0200

    Handle banner update

--- frontend/src/Componets/userProfile/Banner/banner.styles.js
+++ frontend/src/Componets/userProfile/Banner/banner.styles.js
@@ -1,23 +1,22 @@
 import styled from "styled-components";
-import img1 from "../../../Assets/images/b1.png";
+//import img1 from "../../../Assets/images/b1.png";
+
 
 export const BannerElements = styled.div`
   position: relative;
   display: flex;
   width: 100vw;
   height: 22vh;
-  flex-direction: row;
+  background-color: aliceblue;
 `;
 
-export const Img = styled.img.attrs({
-  src: `${img1}`,
-})`
-  display: flex;
+export const Button = styled.button`
   position: absolute;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  background-repeat: no-repeat;
-  max-width: 100%;
-  z-index: 0;
+  background-color: white;
+  position: absolute; /* Add this */
+  left: 85vw; /* Also add this for position: absolute to work properly */
+  top: 15vh;
+  border-radius: 3px;
+  min-width: 10%;
+  display: inline-block;
 `;
