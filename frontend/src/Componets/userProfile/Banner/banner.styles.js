ed7b7419196ac4a40eb20c28fd287ba0de05ee48
Laysa Uchoa <laysa.uchoa@gmail.com>
2023-04-03 11:41:00 +0200

    Add no repeat to banner

--- frontend/src/Componets/userProfile/Banner/banner.styles.js
+++ frontend/src/Componets/userProfile/Banner/banner.styles.js
@@ -5,20 +5,19 @@ export const BannerElements = styled.div`
   position: relative;
   display: flex;
   width: 100vw;
-  height: 20vh;
+  height: 22vh;
   flex-direction: row;
-  
 `;
 
 export const Img = styled.img.attrs({
   src: `${img1}`,
 })`
   display: flex;
-
   position: absolute;
   width: 100%;
   height: 100%;
-  object-fit: cover; /* or object-fit: contain; */
+  object-fit: cover;
+  background-repeat: no-repeat;
   max-width: 100%;
   z-index: 0;
 `;
